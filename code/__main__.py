1a020de7926672dfe60a9e23e72e0aa7044ebbee
wuxinxie <xinxiewu@seas.upenn.edu>
2023-08-15 20:39:09 -0400

    done

--- code/__main__.py
+++ code/__main__.py
@@ -1,8 +1,9 @@
 """
 __maim__.py contains the workflow to run all sub-programs
 """
-import os
+import numpy as np
 from util import *
+from models import *
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -34,14 +35,36 @@ def main(fileurl=None, output=None):
     # 1.2.5 Recommendations for Stores' Sales Growth
     print(f"Q4 Recommendation: Work hard!")
     # 1.2.6 Data Preparation for Modeling
-    df = df[['month', 'store', 'item', 'sales']].groupby(by=['month', 'store', 'item']).sum().reset_index()
-    df.to_csv(os.path.join(output, 'data_final.csv'), index=False)
+    df = df[['month', 'sales']].groupby(by=['month']).sum().reset_index()
+    df.sort_values(by=['month'], inplace=True)
+    df['sales_log'] = np.log2(getattr(df, 'sales'))
+    df['sales_diff'] = getattr(df, 'sales').diff()
+    df['sales_diff2'] = getattr(df, 'sales_diff').diff()
+    df['growth'] = getattr(df, 'sales').diff()/getattr(df, 'sales').shift(1)
+    df['log_growth'] = getattr(df, 'sales_log').diff()/getattr(df, 'sales_log').shift(1)
+    # df.to_csv(os.path.join(output, 'data_final.csv'), index=False)
     print(f"Dataset is READY!\n")
 
     """
     Step 2: Time Series Modeling - Predict 3 months' sales for these 50 different items at 10 stores
     """
-
+    print(f"Time Series Analysis")
+    # 2.1 Stationary Test by ADF
+    adf = adf_test(df=df)
+    print(f"{list(adf[getattr(adf, 'ADF 1%')=='Y']['variable'])} passed ADF 1%, while {list(adf[getattr(adf, 'ADF 1%')=='N']['variable'])} NOT.")
+    # 2.2 ARIMA, with random p, d, q
+    model_res = pd.DataFrame(columns=['variable', 'p', 'd', 'q', 'AIC', 'BIC'])
+    cols, ps, ds, qs = list(adf[getattr(adf, 'ADF 1%')=='Y']['variable']), [1, 2, 3], [1, 2, 3], [1, 2]
+    for col in cols:
+        for p in ps:
+            for d in ds:
+                for q in qs:
+                    model_res = pd.concat([model_res, MyARIMA(df=df, col=col, p=p, d=d, q=q)])
+    aic, bic = model_res[model_res['AIC'] == min(model_res['AIC'])], model_res[model_res['BIC'] == min(model_res['BIC'])]
+    print(f"Best model with AIC is \n{aic}")
+    print(f"Best model with BIC is \n{bic}")
+    model_res.to_csv(os.path.join(output, 'model_res.csv'), index=False)
+    print(f"Project Done!")
 
 if __name__ == '__main__':
     """
--- code/models.py
+++ code/models.py
@@ -1,4 +1,48 @@
 """
 models.py contains tools for Time Series Analysis:
-    1. 
+    1. adf_test: Stationary Test by ADF
+    2. MyARIMA
 """
+import os
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.arima.model import ARIMA
+
+# adf_test()
+def adf_test(df=None, cols=['sales','sales_log', 'sales_diff', 'sales_diff2', 'growth', 'log_growth'], output=r'../public/output', fname='adf_test.csv'):
+    """ Generate ADF Test Result
+
+    Args:
+        df: input dataframe
+
+    Returns:
+        datafram
+    """
+    i, res = 0, pd.DataFrame(columns=['variable', 't-stat', 'p-val', '1%', '5%', '10%', 'ADF 1%', 'ADF 5%', 'ADF 10%'])
+    temp = df.dropna()
+    for col in cols:
+        test = adfuller(getattr(temp, col))
+        res_temp = pd.DataFrame({'variable': col, 't-stat': test[0], 'p-val': test[1],
+                                '1%': test[4]['1%'], '5%': test[4]['5%'], '10%': test[4]['10%'], 
+                                'ADF 1%': 'Y' if test[0] < test[4]['1%'] else 'N', 
+                                'ADF 5%': 'Y' if test[0] < test[4]['5%'] else 'N'
+                                ,'ADF 10%': 'Y' if test[0] < test[4]['10%'] else 'N'}, index=[i])
+        res = pd.concat([res, res_temp])
+        i += 1
+    res.to_csv(os.path.join(output,fname), index=False)
+    return res
+
+# MyARIMA()
+def MyARIMA(df=None, col=None, p=None, d=None, q=None):
+    """ Generate ADF Test Result
+
+    Args:
+        df: input dataframe
+
+    Returns:
+        dataframe
+    """
+    temp = df.dropna()
+    model = ARIMA(getattr(temp, col), order=(p, d, q))
+    res = model.fit()
+    return pd.DataFrame({'variable': col, 'p': p, 'd': d, 'q': q, 'AIC': res.aic, 'BIC': res.bic}, index=[0])
